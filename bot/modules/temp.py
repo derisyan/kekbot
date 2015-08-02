1ba34a4f6ac94e57870a1af654f77f1e24962866
AtaK <AtaK@users.noreply.github.com>
2015-08-02 13:38:08 +0300

    minor fix to temp cmd

--- bot/modules/temp.py
+++ bot/modules/temp.py
@@ -1,10 +1,10 @@
 from core import *
-from subprocess import Popen
+import subprocess
 
 #This command is for the Raspberry Pi
 @command("temp")
 def show_temp(bot, msg, args, pure):
-	process = Popen(("/opt/vc/bin/vcgencmd", "measure_temp"), stdout=subprocess.PIPE)
+	process = subprocess.Popen(("/opt/vc/bin/vcgencmd", "measure_temp"), stdout=subprocess.PIPE)
 	output = process.communicate()[0]
 	out = "current temp is " + output.split("=")[-1]
 	if (not pure) and out:
